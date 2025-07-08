6b8dc451149f16b567b8fa9c19bfe7dfbbc40064
Claudio9905 <cloudpost10@gmail.com>
2025-07-09 00:44:32 +0200

    update file

--- js/webstorage.js
+++ js/webstorage.js
@@ -5,6 +5,7 @@ const inputName = document.getElementById("input1");
 // recupre id dei bottoni
 
 const save = document.getElementById("saveButton");
+const load = document.getElementById("loadButton");
 const remove = document.getElementById("removeButton");
 const memoryKey = "inputName-memory";
 
@@ -15,6 +16,16 @@ save.addEventListener(`click`, function () {
   // salvataggio di questa stringa nel localStorage sulla chiave prefissata
   localStorage.setItem(memoryKey, name);
   alert(`Contenuto Salvato`);
+});
+
+load.addEventListener(`click`, function () {
+  const savedInput = localStorage.getItem(memoryKey);
+  if (savedInput !== null) {
+    inputName.value = savedInput;
+    alert("Contenuto caricato");
+  } else {
+    alert("Nessun contenuto salvato");
+  }
 });
 
 remove.addEventListener(`click`, function () {
